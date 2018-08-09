b9a7e36a78ad32a682f4eeaaf9dbb70a336de4d7
SAI TEJA SUGGULA <32015565+SaiVNR@users.noreply.github.com>
2018-08-09 07:51:56 +0530

    Add files via upload

---
+++ WebDemo/src/main/java/webapp/LoginServlet.java
@@ -0,0 +1,49 @@
+package webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(urlPatterns="/login.do")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		String name = request.getParameter("username");
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello "+ name +"</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
